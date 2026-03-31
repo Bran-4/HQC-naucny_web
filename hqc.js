bb59309595490f8cade6cc76f4511f386b7eb178
Bran-4 <brajurko@gmail.com>
2026-03-31 18:57:56 +0200

    class

--- hqc.js
+++ hqc.js
@@ -1,79 +1,87 @@
+/////////////potrebne pouzit funkcie.js
+
 class HQC {
     constructor(n = 21, gMatrix = []) {
+        // verejne parametre
         this.n = n;
         this.G = gMatrix;
+        this.w=1;
+        this.w_e=1;
+        this.w_r=1;
+        this.h = generate_vect_h(this.n);
+        this.roth=createRotationMatrix(this.h);
+
+        //sukromne parametre BOB
+        this.x = generateSparseVector(this.n, this.w);
+        this.y = generateSparseVector(this.n, this.w);
+        this.y_rotH=[];
+        this.s = []; 
+        this.rot_s=[];
+
+        this.sr2=[];
         
-        // Kľúče a premenné
-        this.h = [];
-        this.x = [];
-        this.y = [];
-        this.s = []; // Verejný kľúč
-        
-        // Šifrovacie medzikroky
-        this.r1 = [];
-        this.r2 = [];
-        this.e = [];
+        // parametre Alica
+        this.r_1 = generateSparseVector(this.n, this.w_r);
+        this.r_2 = generateSparseVector(this.n, this.w_r);
+        this.e = generateSparseVector(this.n,this.w_e);
+        this.r_2_rotH=[];
         this.u = [];
+        this.rot_u=[];
+        
         this.v = [];
+        this.uy=[];
+        this.v_minus_uy=[];
         
         // Správy
         this.m = [1, 0, 1];
-        this.decryptedM = [];
+        this.mG=[];
+
     }
 
-    // Generovanie kľúčov (Alice)
-    generateKeys(w) {
+    // Generovanie parametra h
+    generateH() {
         this.h = generate_vect_h(this.n);
-        this.x = generateSparseVector(this.n, w);
-        this.y = generateSparseVector(this.n, w);
-        
-        const roth = createRotationMatrix(this.h);
-        const pomocny_s = multiplyVectorMatrix(this.y, roth);
-        this.s = addVectors(pomocny_s, this.x);
+        this.roth=createRotationMatrix(this.h);
+    }
+    
+    generateXY(){
+         this.x = generateSparseVector(this.n, this.w);
+        this.y = generateSparseVector(this.n, this.w);
+    }
+    generateE(){
+        this.e=generateSparseVector(this.n,this.w_e);
+    }
+    generateR(){
+        this.r_1 = generateSparseVector(this.n, this.w_r);
+        this.r_2 = generateSparseVector(this.n, this.w_r);
     }
 
-    // Šifrovanie (Bob)
-    encrypt(message, w_r, w_e) {
-        this.m = [...message];
-        this.r1 = generateSparseVector(this.n, w_r);
-        this.r2 = generateSparseVector(this.n, w_r);
-        this.e = generateSparseVector(this.n, w_e);
 
-        const roth = createRotationMatrix(this.h);
-        const rot_s = createRotationMatrix(this.s);
-
-        // u = r1 + r2 * H
-        const pomocny_u = multiplyVectorMatrix(this.r2, roth);
-        this.u = addVectors(pomocny_u, this.r1);
-
-        // v = m*G + r2 * S + e
-        const mG = multiplyVectorMatrix(this.m, this.G);
-        const sr2 = multiplyVectorMatrix(this.r2, rot_s);
-        this.v = addVectors(mG, addVectors(sr2, this.e));
+    // vyratanie parametru S (Bob)
+    vypočitajS() {
+      this.y_rotH=multiplyVectorMatrix(this.y, this.roth);
+        this.s=addVectors(this.y_rotH, this.x);
+    }
+//sifrovanie (ALICA)
+    encrypt(){
+        this.rot_s=createRotationMatrix(this.s);
+        this.r_2_rotH=multiplyVectorMatrix(this.r_2, this.roth);
+        this.u=addVectors(this.r_2_rotH, this.r_1);
+        this.mG=multiplyVectorMatrix(this.m, this.G);
+        this.sr2=multiplyVectorMatrix(this.r_2, this.rot_s);
+        this.v=addVectors(this.mG, addVectors(this.sr2, this.e));    
     }
 
-    // Dešifrovanie (Alice)
+
+    // Dešifrovanie (BOB)
     decrypt() {
-        const rot_u = createRotationMatrix(this.u);
-        const uy = multiplyVectorMatrix(this.y, rot_u);
-        const final = addVectors(this.v, uy);
-
-        // Decoding (opakovací kód podľa dĺžky bloku v G)
-        let result = [];
-        let blockSize = this.n / this.G.length; // v tvojom prípade 21/3 = 7
-        
-        for (let i = 0; i < this.G.length; i++) {
-            let count0 = 0;
-            for (let j = 0; j < blockSize; j++) {
-                if (final[i * blockSize + j] === 0) count0++;
-            }
-            result[i] = (count0 > blockSize / 2) ? 0 : 1;
-        }
-        this.decryptedM = result;
-        return result;
+       this.rot_u= createRotationMatrix(this.u);
+       this.uy = multiplyVectorMatrix(this.y, this.rot_u);
+       this.v_minus_uy=addVectors(this.v, this.uy);
     }
-
-    isReliable(w, we, wr) {
-        return (w + we + wr <= 3);
+    update(){
+        this.vypočitajS();
+        this.encrypt();
+        this.decrypt();
     }
 }
